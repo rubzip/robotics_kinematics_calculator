dd89743b0eb54b3fd7f3aacc37aec34e36d063b2
Rubén <74114026+rubzip@users.noreply.github.com>
2025-11-17 12:03:08 +0100

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 import re
+from sympy import sp
 
 PATTERN = r"([TR])\s*([xyz])\s*\((\w+)\)"
 
@@ -6,10 +7,10 @@ def parse(expr: str, dim: Literal[2, 3] = 3) -> Optional[sy.Matrix]:
     expressions = re.findall(PATTERN, expr)
     if not expressions:
         return None
-    M = sy.eye(dim + 1)
+    M = sp.eye(dim + 1)
     for operator, axis, var in expressions:
         if operator == 'R':
             M = M * get_rotation(var, axis, dim)
         elif operator == 'T':
             M = M * get_translation(var, axis, dim)
-    return sy.simplify(M)
+    return sp.simplify(M)
