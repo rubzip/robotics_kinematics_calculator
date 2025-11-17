1b522df1d5a62dc6109269def510392831b88e73
Rubén <74114026+rubzip@users.noreply.github.com>
2025-11-17 11:53:39 +0100

    Update position.py

--- src/position.py
+++ src/position.py
@@ -3,7 +3,7 @@ import sympy as sp
 def get_position(transformation: sp.Matrix, center: sp.Matrix = None) -> sp.Matrix:
   if center is None:
     center = sp.Zeros(len(transformation), 1)
-  return transformation * center
+  return sp.simplify(transformation * center)
 
 def get_jacobian(transformation: sp.Matrix, symbols: sp.Matrix):
-  return transformation.jacobian(symbols)
+  return sp.simplify(transformation.jacobian(symbols))
